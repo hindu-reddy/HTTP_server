6cd5e83ba1a79e11a075581fcd0322e1343b5c5a
hindu-reddy <hinduja.korrapolu@gmail.com>
2024-10-21 20:38:18 -0700

    Gzip Compression

--- app/server.go
+++ app/server.go
@@ -1,46 +1,117 @@
 package main
 
 import (
+	"bytes"
+	"compress/gzip"
+	"flag"
 	"fmt"
 	"net"
 	"os"
+	"path"
 	"strings"
 )
 
+var filesDir string
+
 func main() {
-	fmt.Println("Logs from your program will appear here!")
+	flag.StringVar(&filesDir, "directory", "./files", "specify a directory where files uploaded to server are saved")
+	flag.Parse()
+	fmt.Println("Saving files to: ", filesDir)
+	fmt.Println("Got args: ", os.Args)
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
-	defer l.Close()
+	fmt.Println("Listening")
 	for {
-		connection, err := l.Accept()
+		conn, err := l.Accept()
+		fmt.Println("Accepted")
 		if err != nil {
 			fmt.Println("Error accepting connection: ", err.Error())
 			continue
 		}
-		go handleConnection(connection)
+		go handleConnection(conn)
+		fmt.Println("Handling started")
 	}
 }
-func handleConnection(connection net.Conn) {
-	defer connection.Close()
-	requestBuffer := make([]byte, 1024)
-	n, err := connection.Read(requestBuffer)
-	if err != nil {
-		fmt.Println("Failed to read the request:", err)
-		return
+func handleConnection(conn net.Conn) {
+	buffer := make([]byte, 1024)
+	n, _ := conn.Read(buffer)
+	request := string(buffer[:n])
+	resp := dispatch(parseRequest(request))
+	conn.Write([]byte(resp))
+	fmt.Println("Handled")
+}
+func parseRequest(request string) (method string, path string, headers map[string]string, body string) {
+	fmt.Println("~~~ request ~~~\n", request)
+	requestParts := strings.Split(request, "\r\n")
+	requestLine := requestParts[0]
+	lineParts := strings.Split(requestLine, " ")
+	method = lineParts[0]
+	path = lineParts[1]
+	body = requestParts[len(requestParts)-1]
+	headers = make(map[string]string)
+	for i := 1; i < len(requestParts)-1; i++ {
+		key, value, _ := strings.Cut(requestParts[i], ":")
+		headers[strings.ToLower(strings.Trim(key, " "))] = strings.Trim(value, " ")
 	}
-	fmt.Printf("Request: %s\n", requestBuffer[:n])
-	request := string(requestBuffer[:n])
-	path := strings.Split(request, " ")[1]
-	if path == "/" {
-		connection.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-	} else if strings.Split(path, "/")[1] == "echo" {
-		message := strings.Split(path, "/")[2]
-		connection.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(message), message)))
+	return // named result parameters
+}
+func dispatch(method string, urlPath string, headers map[string]string, body string) string {
+	if method != "GET" {
+		if method == "POST" && strings.HasPrefix(urlPath, "/files/") {
+			fname := urlPath[len("/files/"):]
+			f, err := os.Create(path.Join(filesDir, fname))
+			if err != nil {
+				return "HTTP/1.1 422 Unprocessable Entity\r\n\r\n"
+			}
+			defer f.Close()
+			f.WriteString(body)
+			return "HTTP/1.1 201 Created\r\n\r\n"
+		} else {
+			return "HTTP/1.1 405 Method Not Allowed\r\n\r\n"
+		}
+	}
+	if urlPath == "/" || urlPath == "/index.html" {
+		return "HTTP/1.1 200 OK\r\n\r\n"
+	} else if strings.HasPrefix(urlPath, "/echo/") {
+		text := urlPath[len("/echo/"):]
+		return contentResponse(text, "text/plain", headers)
+	} else if urlPath == "/user-agent" {
+		agent := headers["user-agent"]
+		return contentResponse(agent, "text/plain", headers)
+	} else if strings.HasPrefix(urlPath, "/files/") {
+		fname := urlPath[len("/files/"):]
+		dat, err := os.ReadFile(path.Join(filesDir, fname))
+		if err != nil {
+			return "HTTP/1.1 404 Not Found\r\n\r\n"
+		}
+		return contentResponse(string(dat), "application/octet-stream", headers)
 	} else {
-		connection.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+		return "HTTP/1.1 404 Not Found\r\n\r\n"
 	}
+}
+func contentResponse(content string, contentType string, requestHeaders map[string]string) string {
+	values, ok := requestHeaders["accept-encoding"]
+	compression := ""
+	if ok {
+		for _, elem := range strings.Split(values, ",") {
+			if strings.TrimSpace(elem) == "gzip" {
+				var buffer bytes.Buffer
+				w := gzip.NewWriter(&buffer)
+				w.Write([]byte(content))
+				w.Close()
+				content = buffer.String()
+				compression = "Content-Encoding: gzip\r\n"
+				break
+			}
+		}
+	}
+	return "HTTP/1.1 200 OK\r\n" +
+		compression +
+		"Content-Type: " + contentType + "\r\n" +
+		"Content-Length: " + fmt.Sprint(len(content)) +
+		"\r\n" +
+		"\r\n" + content
 }
